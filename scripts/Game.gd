ab97ac69b2ed29df923ea1779d14c1a4a7942794
Sebastian <sebastian-raabe@hotmail.de>
2025-09-24 16:53:48 +0200

    HighscoreListe am Ende
    und paar kleine grafische tweeks

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -6,14 +6,18 @@ extends Node2D
 @onready var next_round_time_label: Label = $PauseScreen/NextRoundTimeLabel
 @onready var retry_button: Button = $EndScreen/RetryButton
 @onready var timer_label: Label = $GameScreen/TimerLabel
-#@onready var score_label: Label = $ScoreLabel
+@onready var score_label: Label = $ScoreLabel
 @onready var final_score_label: Label = $EndScreen/FinalScore
 @onready var end_screen: CanvasLayer = $EndScreen
 @onready var game_timer: Timer = $GameTimer
 @onready var box_sack: Sprite2D = $GameScreen/BoxSack
-@onready var game_camera: Camera2D = $GameCamera
+@onready var game_camera: Camera2D = $GameScreen/GameCamera
+@onready var highscore_label: Label = $EndScreen/HighscoreLabel
 
+
+var highscores: Array[int] = []
 var score: int = 0
+@warning_ignore("shadowed_global_identifier")
 var round: int = 0
 var rounds_total: int = 3
 var round_durations: Array = []
@@ -21,6 +25,8 @@ var next_round_time: int = 0
 var game_active: bool = false
 var sack_stages: Array[Texture2D] = []  # Texturen für Schaden
 var highscore: int = 0
+var round_count: int = 0
+
 
 func _ready() -> void:
 	randomize()
@@ -40,7 +46,9 @@ func _ready() -> void:
 	load("res://assets/BXS3.png"),
 	load("res://assets/BoxSackBase.png")
 ]
-#box_sack.texture = sack_stages[0]
+	box_sack.texture = sack_stages[0]
+	game_camera.make_current()
+
 
 func _show_pause_screen() -> void:
 	game_active = false
@@ -58,7 +66,10 @@ func _show_pause_screen() -> void:
 	else:
 		pause_screen.visible = false
 		_show_end_screen()
-
+	if score > 100:
+		# Effekt / Animation optional
+		box_sack.queue_free()  # entfernt den Sack komplett
+		
 func _start_round() -> void:
 	game_active = true
 	pause_screen.visible = false
@@ -79,9 +90,34 @@ func _process(_delta: float) -> void:
 		timer_label.text = "Time: %.1f" % game_timer.time_left
 
 func _on_game_timer_timeout() -> void:
-	if game_active:
-		game_active = false
+	if not game_active:
+		return
+
+	game_active = false
+	round_count += 1
+	if round_count <= sack_stages.size():
+		box_sack.texture = sack_stages[round_count - 1]
+
+	if round_count < rounds_total:
+		# Nächste Runde vorbereiten
 		_show_pause_screen()
+	else:
+		# Finaler Endscreen nach letzter Runde
+		end_screen.visible = true
+		final_score_label.text = "Final Score: %d" % score
+		timer_label.visible = false
+
+		# Jetzt erst Highscore hinzufügen
+		highscores.append(score)
+		highscores.sort()
+		highscores.reverse()
+
+		# Anzeige (z. B. Top 5)
+		var top_scores = highscores.slice(0, 5)
+		var list_text := "Highscores:\n"
+		for i in range(top_scores.size()):
+			list_text += "%d. %d\n" % [i + 1, top_scores[i]]
+		highscore_label.text = list_text
 
 func _input(event: InputEvent) -> void:
 	if not game_active:
@@ -106,14 +142,15 @@ func _show_end_screen() -> void:
 	timer_label.visible = false
 #	score_label.visible = false
 
+
 func _on_retry_button_pressed():
 	get_tree().reload_current_scene()	
 
 	
 func camera_shake() -> void:
-	var rand_x = randf_range(-5.0, 5.0)
-	var rand_y = randf_range(-5.0, 5.0)
-	game_camera.offset = Vector2(rand_x, rand_y)
-	# Nach sehr kurzer Zeit zurücksetzen
-	await get_tree().create_timer(0.05).timeout
+	for i in 3: # 3 schnelle Wackler
+		var rand_x = randf_range(-10.0, 10.0)
+		var rand_y = randf_range(-10.0, 10.0)
+		game_camera.offset = Vector2(rand_x, rand_y)
+		await get_tree().create_timer(0.4).timeout
 	game_camera.offset = Vector2.ZERO
