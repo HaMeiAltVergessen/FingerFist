d60fa4e595701d2b1a42263cc91035cd0c5f3286
Luca Mahncke <scooper@posteo.de>
2025-10-08 22:00:05 +0200

    Coin Spawning hinzugefügt
    Moneyrain $$$, #VibeCoding #Nur200Zeilen #TomIstEinHaterForNoReason #PerplexityWillstDuMichHeiraten #AISlurp #TylerTheCreatorImHintergrundZuHören #FrrrankenGameJam

--- scripts/Coins.gd
+++ scripts/Coins.gd
@@ -1,9 +1,17 @@
 extends Area2D
-
+## Features:
+# Muss noch länger laufen, mehr Coins, nur wenn Game auch aktiv ist, Sounds wenn angeklickt, Counter für Münzen
 @onready var sprite: Sprite2D = $Sprite2D
+var acceleration: float = 600.0 # Standardwert, wird vom Spawner gesetzt!
+var velocity: float = 0.0
 
 func _ready() -> void:
 	connect("input_event", Callable(self, "_on_input_event"))
+	connect("area_entered", Callable(self, "_on_area_entered"))
+
+func _process(delta: float) -> void:
+	velocity += acceleration * delta
+	position.y += velocity * delta
 
 func _on_input_event(viewport, event, shape_idx) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -12,6 +20,6 @@ func _on_input_event(viewport, event, shape_idx) -> void:
 		Global.save_progress()
 		queue_free()
 
-
-func _on_shop_button_pressed() -> void:
-	pass # Replace with function body.
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "Ground":
+		queue_free()
---
+++ scripts/coinspawner.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var coin_scene: PackedScene
+@export var coins_to_spawn: int = 10
+@export var min_acceleration: float = 400.0
+@export var max_acceleration: float = 900.0
+@export var min_delay: float = 0.2
+@export var max_delay: float = 1.2
+
+var coins_spawned: int = 0
+
+func _ready():
+	spawn_next_coin()
+
+func spawn_next_coin():
+	if coins_spawned >= coins_to_spawn:
+		return
+	# Coin instanziieren
+	var coin = coin_scene.instantiate()
+
+	# Zufällige X-Position am oberen Rand
+	var viewport_width = get_viewport().size.x
+	coin.global_position.x = randi_range(16, viewport_width - 16)
+	coin.global_position.y = 0
+
+	# Zufällige Beschleunigung/Speed der einzelnen Coin
+	coin.acceleration = randf_range(min_acceleration, max_acceleration)
+
+	add_child(coin)
+
+	coins_spawned += 1
+
+	# Timer für verzögertes nächstes Spawnen
+	var delay = randf_range(min_delay, max_delay)
+	await get_tree().create_timer(delay).timeout
+	spawn_next_coin()
